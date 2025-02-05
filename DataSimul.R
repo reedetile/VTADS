68853929bda06b05cbb325af87bf84511c63ebd0
Reed Scott <rscott1294@gmail.com>
2025-02-04 21:46:08 -0500

    working on simulating data

--- DataSimul.R
+++ DataSimul.R
@@ -4,7 +4,10 @@
 #RCS
 
 #Initialize -----------------------------------------
-
+rm(list=ls())
+library(unmarked)
+library(MuMIn)
+library(ggplot2)
 # Load functions--------------------------------------
 
 
@@ -12,41 +15,48 @@
 
 
 # Program Body------------------------------------------
-# Initialize variables#
+# Initialize variables
 set.seed(1234)
-sites <- 12
-ponds <- 24
-visits <- 3
-frogs_per_pond <- visits * 12
-tests_per_frog <-  3
-tests_per_pond <- frogs_per_pond * tests_per_frog
+ponds <- 24 #number of sites
+frogs <- 2 #number of frogs PER VISIT
+visits <- 3 #number of visits to each site
+tests <- 3 #number of tests per frog
+total_frogs <- frogs*visits
+total_tests <- frogs*visits*tests #total number of tests for each pond for the season
 
-#creating occupancy data#
-#Create fake site level covar
-temp <- rnorm(n = ponds, mean = 30, sd = 2)
-error <- rnorm(ponds)
-corr <- 0.75
-var <- corr * temp + sqrt(1 - corr^2) * rnorm(ponds)
-psi <- pnorm(var, mean = 0, sd = 1)
+# Create an empty dataframe for the detection data
+det <- matrix(NA, nrow = ponds, ncol = total_tests)
+
+# Create a site lvl covar
+site_covs <- data.frame(temp = rnorm(ponds),betadiv = rnorm(ponds))
+
+umf <- unmarkedFrameGOccu(y = det, siteCovs = site_covs, numPrimary = total_frogs)
+head(umf)
+simulate(umf, model = goccu,  psiformula = ~temp, phiformula =~ betadiv, pformula =~ 1)
+coeff <- list(psi = c(0,-2), 
+              phi = c(0,5), 
+              det = 0)
+sim <- simulate(umf, model = goccu,  psiformula = ~temp, phiformula = ~betadiv, pformula =~ 1, coefs = coeff)
+head(sim[[1]])
+tempXpsiphiXbeta <- goccu(psiformula = ~temp, phiformula = ~betadiv, pformula =~ 1, data = sim[[1]])
+summary(tempXpsiphiXbeta)
+temp <- data.frame(temp = seq(from = -2, to = 2, by = 0.1))
+temppredict <- unmarked::predict(tempXpsiphiXbeta, type = "psi", newdata = temp, appendData = T)
+ggplot(data = temppredict, aes(x= temp, y= Predicted))+
+  geom_line(linewidth = 1.5) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  scale_colour_brewer(palette = "Set1") +
+  xlab("Temperature") + 
+  ylab("Psi")+
+  theme_classic()
 
 
-
-occupancy <- rbinom(n = ponds, size = 1, prob = 0.75)
-frog_pos <- data.frame(matrix(nrow = ponds, ncol = frogs_per_pond))
-test_pos <-  data.frame(matrix(nrow = ponds, ncol = tests_per_pond))
-
-for (i in 1:length(occupancy)) {
-  frog_pos[i,1:ncol(frog_pos)] <- if(occupancy[[i]] == 1){
-    rbinom(n = frogs_per_pond, size = 1, prob = 0.65)} else{
-      rep(0, times = frogs_per_pond)}
-}
-
-for (i in 1:length(occupancy)) {
-  for(j in 1:ncol(frog_pos)) {
-    assign(paste("frog",j,sep = "_"), if(frog_pos[i,j] == 1){
-      rbinom(n = tests_per_frog, size = 1, prob = 0.95)} else{
-        rep(0, times = tests_per_frog)})
-    det <- unlist(lapply(objects(pattern = "frog_\\d+"), get))
-    test_pos[i,1:ncol(test_pos)] <- det 
-  }
-}
+betadiv <- data.frame(betadiv = seq(from = -2, to = 2, by = 0.1))
+betapredict <- unmarked::predict(tempXpsiphiXbeta, type = "phi", newdata = betadiv, appendData = T)
+ggplot(data = betapredict, aes(x= betadiv, y= Predicted))+
+  geom_line(linewidth = 1.5) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  scale_colour_brewer(palette = "Set1") +
+  xlab("Beta diversity") + 
+  ylab("Psi")+
+  theme_classic()
