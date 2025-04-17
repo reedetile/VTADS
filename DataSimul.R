384c0246b00599d9b27663611a6a4646b675c66f
Reed Scott <rscott1294@gmail.com>
2025-04-17 10:23:27 -0400

    Worked on data simulation + graphing

--- DataSimul.R
+++ DataSimul.R
@@ -8,11 +8,13 @@ rm(list=ls())
 library(unmarked)
 library(MuMIn)
 library(ggplot2)
+library(patchwork)
 # Load functions--------------------------------------
 
 
 # Global Variables-------------------------------------
-
+repo <- getwd()
+graphs <- paste(repo, "/Graphs", sep = "")
 
 # Program Body------------------------------------------
 # Initialize variables
@@ -65,7 +67,7 @@ ggplot(data = betapredict, aes(x= betadiv, y= Predicted))+
 
 # Okay it works! so lets simulate our full model
 
-# Lets simulate and effect of beta on occupancy, Spp on theta, and alpha on detection
+# Lets simulate and effect of beta on occupancy and on theta, and alpha on detection
 # but we'll fit + dredge the full model and see if it picks the top model
 # Create an empty dataframe for the detection data
 det <- matrix(NA, nrow = ponds, ncol = total_tests)
@@ -73,48 +75,49 @@ det <- matrix(NA, nrow = ponds, ncol = total_tests)
 # Create a site lvl covar
 site_covs <- data.frame(betadiv = rnorm(ponds),
                         alpha = rnorm(ponds))
-Spp <-  data.frame(matrix(sample(c("Spp1","Spp2","Spp3","Spp4"), size = total_frogs*ponds, replace = T), nrow = ponds, ncol = total_frogs))
-Spp <-  data.frame(lapply(Spp,as.factor))
+beta <- rnorm(ponds)
+beta_effect <-  data.frame(matrix(rep(beta, each = total_frogs), nrow = ponds, ncol = total_frogs, byrow = T))
 
-avail_covs <- list(betadiv = data.frame(data = matrix(rep(site_covs$betadiv, each = total_frogs), nrow = ponds, ncol = total_frogs, byrow = T)),
-                   alpha = data.frame(data = matrix(rep(site_covs$alpha, each = total_frogs), nrow = ponds, ncol = total_frogs, byrow = T)),
-                   R0P = data.frame(data = matrix(rep(site_covs$R0P, each = total_frogs), nrow = ponds, ncol = total_frogs, byrow = T))) 
-names(avail_covs$R0P) <- c("Ind1","Ind2","Ind3","Ind4","Ind5","Ind6")
-Spp_dets <- NULL
-for(i in 1:ncol(Spp)){
-  Spp_temp <- matrix(rep(Spp[,i], each = tests), nrow = ponds, ncol = tests, byrow = T)
-  Spp_dets <- cbind(Spp_dets, Spp_temp)
+avail_covs <- list(betadiv = data.frame(data = matrix(rep(site_covs$betadiv, each = total_frogs),
+                                                      nrow = ponds, ncol = total_frogs, byrow = T)),
+                   alpha = data.frame(data = matrix(rep(site_covs$alpha, each = total_frogs), 
+                                                    nrow = ponds, ncol = total_frogs, byrow = T))) 
+names(avail_covs$betadiv) <- c("Ind1","Ind2","Ind3","Ind4","Ind5","Ind6")
+beta_dets <- NULL
+for(i in 1:ncol(beta_effect)){
+  beta_temp <- matrix(rep(beta_effect[,i], each = tests), nrow = ponds, ncol = tests, byrow = T)
+  beta_dets <- cbind(beta_dets, beta_temp)
 }
-Spp_dets <- data.frame(lapply(data.frame(Spp_dets),as.factor))    
+theta_dets <- data.frame(beta_dets)    
     
-test_covs <- list(betadiv = data.frame(data = matrix(rep(site_covs$betadiv, each = total_tests), nrow = ponds, ncol = total_tests, byrow = T)),
-                  alpha = data.frame(data = matrix(rep(site_covs$alpha, each = total_tests), nrow = ponds, ncol = total_tests, byrow = T)),
-                  R0P = data.frame(data = matrix(rep(site_covs$R0P, each = total_tests), nrow = ponds, ncol = total_tests, byrow = T)),
-                  Spp = Spp_dets)
+test_covs <- list(betadiv = data.frame(data = matrix(rep(site_covs$betadiv, each = total_tests), 
+                                                     nrow = ponds, ncol = total_tests, byrow = T)),
+                  alpha = data.frame(data = matrix(rep(site_covs$alpha, each = total_tests), 
+                                                   nrow = ponds, ncol = total_tests, byrow = T)),
+                  beta_dets = beta_dets)
 for(i in 1:length(test_covs)){
   for (j in 1:ncol(test_covs$betadiv)){
     names(test_covs[[i]])[[j]] <- paste("test",i, sep = "")
   }
 }
-umf <- unmarkedFrameGOccu(y = det, siteCovs = site_covs, obsCovs = test_covs, yearlySiteCovs = avail_covs, numPrimary = total_frogs)
+umf <- unmarkedFrameGOccu(y = det, siteCovs = site_covs, obsCovs = test_covs, yearlySiteCovs = avail_covs, 
+                          numPrimary = total_frogs)
 head(umf)
-simulate(umf, model = goccu,  psiformula = ~betadiv, phiformula =~ Spp, pformula =~ alpha)
+simulate(umf, model = goccu,  psiformula = ~betadiv, phiformula =~ betadiv, pformula =~ alpha)
 coeff <- list(psi = c(0,-0.7), 
-              phi = c(0,-0.9,0.8,0.5), 
-              det = c(0,0.75))
-sim2 <- simulate(umf, model = goccu,  psiformula = ~betadiv, phiformula = ~Spp, pformula =~ alpha, coefs = coeff)
+              phi = c(0,-0.9), 
+              det = c(0,-0.75))
+sim2 <- simulate(umf, model = goccu,  psiformula = ~betadiv, phiformula = ~betadiv, pformula =~ alpha, coefs = coeff)
 
 #null model
 null.mod <- goccu(psiformula = ~1, phiformula = ~1, pformula = ~1,data = sim2[[1]])
 #Models for psi
 psi.beta <- goccu(psiformula = ~betadiv, phiformula = ~1, pformula = ~1, data = sim2[[1]])
 psi.alpha <- goccu(psiformula = ~alpha, phiformula = ~1, pformula =~ 1, data = sim2[[1]])
-psi.R0P <- goccu(psiformula = ~R0P, phiformula = ~1, pformula =~ 1, data = sim2[[1]])
 
 fitlist.psi <- fitList("nullpsi" = null.mod,
                    "betapsi" = psi.beta,
-                   "alphapsi" = psi.alpha,
-                   "R0Ppsi" = psi.R0P)
+                   "alphapsi" = psi.alpha)
 
 modSel(fitlist.psi)
 summary(psi.beta)
@@ -122,19 +125,94 @@ summary(psi.beta)
 #Models for theta
 theta.beta <- goccu(psiformula = ~1, phiformula = ~betadiv, pformula = ~1, data = sim2[[1]])
 theta.alpha <- goccu(psiformula = ~1, phiformula = ~alpha, pformula = ~1, data = sim2[[1]])
-theta.R0P <- goccu(psiformula = ~1, phiformula = ~R0P, pformula = ~1, data = sim2[[1]])
-theta.spp <- goccu(psiformula = ~1, phiformula = ~Spp, pformula = ~1, data = sim2[[1]])
 
-fitlist.theta <- fitList("beta" = theta.beta,
-                         "alpha" = theta.alpha,
-                         "R0P" = theta.R0P,
-                         "Spp" = theta.spp)
+fitlist.theta <- fitList("null" = null.mod,
+                         "beta" = theta.beta,
+                         "alpha" = theta.alpha)
 modSel(fitlist.theta)
 
-###########################################################################################
-# Creating actual simulations based on potential hypotheses
-##########################################################################################
+# models for p
+p.beta <- goccu(psiformula = ~1, phiformula = ~1, pformula = ~betadiv, data = sim2[[1]])
+p.alpha <- goccu(psiformula = ~1, phiformula = ~1, pformula = ~alpha, data = sim2[[1]])
 
-### First we'll need to create a full dataframe of psi, theta, and p covariates
+fitlist.theta <- fitList("null" = null.mod,
+                         "beta" = p.beta,
+                         "alpha" = p.alpha)
+modSel(fitlist.theta)
 
-#Theta
+# Eventually I'm going to need to create all 27 possible models (so far... could do an interaction of beta + alpha?)
+# Will also need to learn how to do model avg in unmarked
+# However for right now I just want to do some plotting. So let plot the following
+
+#Create df's for predictions
+alpha <- data.frame(alpha = seq(from = -2, to = 2, by = 0.1))
+beta <- data.frame(betadiv = seq(from = -2, to = 2, by = 0.1))
+
+# psi ~ alpha
+
+alphapredict <- unmarked::predict(psi.alpha, type = "psi", newdata = alpha, appendData = T)
+PsiXAlpha <- ggplot(data = alphapredict, aes(x= alpha, y= Predicted))+
+  geom_line(linewidth = 1.5) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  scale_colour_brewer(palette = "Set1") +
+  xlab("Alpha diversity") +
+  ylab("Psi")+
+  theme_classic()
+PsiXAlpha
+# psi ~ beta
+betapredict <- unmarked::predict(psi.beta, type = "psi", newdata = beta, appendData = T)
+PsiXBeta <- ggplot(data = betapredict, aes(x= betadiv, y= Predicted))+
+  geom_line(linewidth = 1.5) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  scale_colour_brewer(palette = "Set1") +
+  xlab("Beta diversity") +
+  ylab("Psi")+
+  theme_classic()
+PsiXBeta
+
+# theta ~ alpha
+alpha_theta_predict <- unmarked::predict(theta.alpha, type = "phi", newdata = alpha, appendData = T)
+thetaXAlpha <- ggplot(data = alpha_theta_predict, aes(x= alpha, y= Predicted))+
+  geom_line(linewidth = 1.5) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  scale_colour_brewer(palette = "Set1") +
+  xlab("Alpha diversity") +
+  ylab("Prevalence")+
+  theme_classic()
+thetaXAlpha
+# theta ~ beta
+beta_theta_predict <- unmarked::predict(theta.beta, type = "phi", newdata = beta, appendData = T)
+thetaXBeta <- ggplot(data = beta_theta_predict, aes(x= betadiv, y= Predicted))+
+  geom_line(linewidth = 1.5) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  scale_colour_brewer(palette = "Set1") +
+  xlab("Beta diversity") +
+  ylab("Prevalence")+
+  theme_classic()
+thetaXBeta
+# p ~ alpha
+alpha_p_predict <- unmarked::predict(p.alpha, type = "det", newdata = alpha, appendData = T)
+pXAlpha <- ggplot(data = alpha_p_predict, aes(x= alpha, y= Predicted))+
+  geom_line(linewidth = 1.5) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  scale_colour_brewer(palette = "Set1") +
+  xlab("Alpha diversity") +
+  ylab("Infection intensity")+
+  theme_classic()
+pXAlpha
+# p ~ beta
+beta_p_predict <- unmarked::predict(p.beta, type = "det", newdata = beta, appendData = T)
+pXBeta <- ggplot(data = beta_p_predict, aes(x= betadiv, y= Predicted))+
+  geom_line(linewidth = 1.5) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  scale_colour_brewer(palette = "Set1") +
+  xlab("Beta diversity") +
+  ylab("Infection Intensity")+
+  theme_classic()
+pXBeta
+
+# put them all together!
+biodivPlots <- (PsiXAlpha + thetaXAlpha + pXAlpha)/(PsiXBeta + thetaXBeta + pXBeta)
+biodivPlots
+setwd(graphs)
+ggsave(filename = "biodivPlots.png", plot = biodivPlots)
