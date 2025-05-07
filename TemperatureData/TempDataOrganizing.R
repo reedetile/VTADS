9f16ace241e7506cef4911e2578e1b49581a2e75
Reed Scott <rscott1294@gmail.com>
2025-05-06 21:25:04 -0400

    created df that summarizes temp data for each site

--- TemperatureData/TempDataOrganizing.R
+++ TemperatureData/TempDataOrganizing.R
@@ -79,10 +79,34 @@ for(i in 1:length(Temperature_list)){
 }
   
 setwd(paste(repo, "/Data", sep = ""))
-load("biodiversityData.RData")
-SwabData <- read.csv("SwabDataMaster.csv")
-SwabData <- filter(SwabData, SiteID != "")
-unique(SwabData$SiteID)
+dipnet <- read.csv("DipnetDataMaster.csv")
+dipnet <- filter(dipnet, SiteID != "")
+unique(dipnet$SiteID)
+
+#lets start by limiting to only the variables we care about
+dipnet$Sweep <- as.numeric(dipnet$Sweep)
+dipnet2 <- dipnet %>% select(c(ComplexID:SiteID,Date,Sweep,AB:RSN))
+dipnet2 <- dipnet2 %>% filter(Sweep < 19) #taking only first 18 sweep to keep sites equal
+
+dipnet2$Date <- mdy(dipnet2$Date)
+unique(dipnet2$Date)
+
+dipnet_sum <- dipnet2 %>% group_by(ComplexID, PondID, SiteID, Date) %>% 
+  dplyr::summarize(AB = sum(AB),
+                   AT = sum(AT),
+                   FT = sum(FT),
+                   GT = sum(GT),
+                   GF = sum(GF),
+                   MF = sum(MF),
+                   LF = sum(LF),
+                   PF = sum(PF),
+                   SP = sum(SP),
+                   WF = sum(WF),
+                   RSN = sum(RSN))
+dipnet_sum <- arrange(dipnet_sum, SiteID, Date)
+dipnet_sum$Visit <- rep(1:3,times = length(unique(dipnet_sum$SiteID))*2)
+SurveyDates <- dipnet_sum %>% select(c(SiteID, Date, Visit))
+SurveyDates$Year <- year(SurveyDates$Date)
 
 
 
@@ -90,102 +114,46 @@ Survey <- c("1","2","3")
 Year <- c("2022","2024")
 num_visits <- length(Survey)*length(Year)
 AvgTempsList <- vector("list", length = length(Temperature_list))
+
 for(i in 1:length(Temperature_list)){
   Site_list <- vector("list", length = length(Year))
   Site_df <- Temperature_list[[i]][[1]]
   Site_df$Temp <- as.numeric(Site_df$Temp)
   name <- names(Temperature_list)[i]
-  print(paste("i = ", i))
+  site_ls <- vector("list", length = length(Year))
   for (j in 1:length(Year)) {
-    print(paste("j = ", j))
-    surv_list <- vector("list", length = length(Survey))
+    surv_df <- data.frame(matrix(nrow = 3, ncol = 3))
+    colnames(surv_df) <- c("Year", "Survey", "mean_temp")
     for (k in 1:length(Survey)) {
-      print(paste("k = ", k))
-      surv_subset <- SwabData %>% filter(SiteID ==  Temperature_list[[i]][[2]] & 
-                                           Survey.Number == Survey[[j]] & 
+      surv_subset <- SurveyDates %>% filter(SiteID ==  Temperature_list[[i]][[2]] & 
+                                           Visit == Survey[[k]] & 
                                            Year == Year[[j]])
-      survey.date <- mdy(unique(surv_subset$Date))
+      survey.date <- surv_subset$Date
       TwoWeeks <- survey.date - days(14)
       time_interval <- interval(TwoWeeks, survey.date)
       data <- Site_df %>% filter(Date %within% time_interval)
       Site <- name
-      Year_visit <- Year[[j]]
-      survey_visit <- Survey[[k]]
-      mean_temp <- mean(data$Temp)
-      surv_list[[j]] <- list(Year_visit, survey_visit, mean_temp)
+      surv_df$Year <- Year[[j]]
+      surv_df[k,2] <- Survey[[k]]
+      surv_df[k,3] <- mean(data$Temp)
     }
-    Site_Year <- paste(name, Year[[j]], sep = "")
-    Site_list[[j]] <- assign(Site_Year, surv_list)
+    site_ls[[j]] <- surv_df
   }
-  AvgTempsList[[i]] <- assign(name, Site_list)
+  AvgTempsList[[i]] <- assign(name, site_ls)
+}
+
+names(AvgTempsList) <- Sites
+
+AvgTempsList2 <- vector("list", length = length(AvgTempsList))
+names(AvgTempsList2) <- names(AvgTempsList)
+for (i in 1:length(AvgTempsList)) {
+  AvgTempsList2[[i]] <-  do.call(rbind, AvgTempsList[[i]])
 }
 
 
-surv_subset <- SwabData %>% filter(SiteID ==  Temperature_list[[7]][[2]] & 
-                                     Survey.Number == Survey[[1]] & 
-                                     Year == Year[[1]])
-survey.date <- mdy(unique(surv_subset$Date))
-
-# Audobon
-Audobon_list <- vector("list", length = length(Year))
-for(i in 1:length(Year)){
-  surv_list <- vector("list", length = length(Survey))
-  for(j in 1:length(Survey)){
-    surv_subset <- SwabData %>% filter(SiteID == '02-01' & Survey.Number == Survey[[j]] & Year == Year[[i]])
-    survey.date <- mdy(unique(surv_subset$Date))
-    TwoWeeks <- survey.date - days(14)
-    time_interval <- interval(TwoWeeks, survey.date)
-    data <- Audobon[[1]] %>% filter(Date %within% time_interval)
-    Site <- "Audobon"
-    Year_visit <- Year[[i]]
-    survey_visit <- Survey[[j]]
-    mean_temp <- mean(data$Temp)
-    surv_list[[j]] <- c(Year_visit, survey_visit, mean_temp)
-  }
-  Audobon_Year <- paste("Audobon", Year[[i]])
-  Audobon_list[[i]] <- assign(Audobon_Year, surv_list)
-}
-    
-    
-# Birds of Vermont
-BirdsOfVT$Temp <- as.numeric(BirdsOfVT$Temp)
-BirdsOfVT_list <- vector("list", length = length(Year))
-for(i in 1:length(Year)){
-  surv_list <- vector("list", length = length(Survey))
-  for(j in 1:length(Survey)){
-    surv_subset <- SwabData %>% filter(SiteID == '01-01' & Survey.Number == Survey[[j]] & Year == Year[[i]])
-    survey.date <- mdy(unique(surv_subset$Date))
-    TwoWeeks <- survey.date - days(14)
-    time_interval <- interval(TwoWeeks, survey.date)
-    data <- BirdsOfVT %>% filter(Date %within% time_interval)
-    Site <- "BirdsOfVT"
-    Year_visit <- Year[[i]]
-    survey_visit <- Survey[[j]]
-    mean_temp <- mean(data$Temp)
-    surv_list[[j]] <- c(Year_visit, survey_visit, mean_temp)
-  }
-  BirdsOfVT_Year <- paste("BirdsOfVT", Year[[i]])
-  BirdsOfVT_list[[i]] <- assign(BirdsOfVT_Year, surv_list)
-}
 
 
-# Camp Johson-02
-CampJ02_list <- vector("list", length = length(Year))
-for(i in 1:length(Year)){
-  surv_list <- vector("list", length = length(Survey))
-  for(j in 1:length(Survey)){
-    surv_subset <- SwabData %>% filter(SiteID == '10-02' & Survey.Number == Survey[[j]] & Year == Year[[i]])
-    survey.date <- mdy(unique(surv_subset$Date))
-    TwoWeeks <- survey.date - days(14)
-    time_interval <- interval(TwoWeeks, survey.date)
-    data <- CampJohnson-2 %>% filter(Date %within% time_interval)
-    Site <- "CampJ02"
-    Year_visit <- Year[[i]]
-    survey_visit <- Survey[[j]]
-    mean_temp <- mean(data$Temp)
-    surv_list[[j]] <- c(Year_visit, survey_visit, mean_temp)
-  }
-  CampJ02_Year <- paste("CampJ02", Year[[i]])
-  CampJ02_list[[i]] <- assign(CampJ02_Year, surv_list)
-}
+SumTempData <- bind_rows(AvgTempsList2, .id="id")
 
+setwd(paste(repo, "/Data", sep = ""))
+saveRDS(SumTempData, file = "SumTempData.rds")
